5899db967ebf478f35b52a44578f9faf44bc4cf6
johmackell <129639439+johmackell@users.noreply.github.com>
2025-02-22 00:37:42 +1100

    update intro to FT

--- main.py
+++ main.py
@@ -12,8 +12,10 @@
 
 from manim import *
 
-FOURIER_TRANSFORM = r"F(\omega)=\int_{-\infty}^{\infty}f(t)e^{-i\omega t} dt"
+FOURIER_TRANSFORM = r"F(\omega)=\int_{-\infty}^{\infty}f(t)e^{-i\omega t} \ dt"
 INVERSE_FOURIER   = r"f(t)=\int_{-\infty}^{\infty}F(\nu)e^{i\omega t} d\omega"
+
+EULERS_FORMULA = r"e^{i\theta }=\cos{\theta }+i\sin{\theta }"
 
 C7_CHORD = lambda x: np.sin(262*x) + np.sin(330*x) + np.sin(392*x) + np.sin(494*x)
 FSM7_CHORD = lambda x: np.sin(370*x) + np.sin(466*x) + np.sin(554*x) + np.sin(698*x)
@@ -155,7 +157,7 @@ class HistoryOfIntegration(Scene):
         self.wait(2)
         self.pol_v = ValueTracker(8)
         pol5 = RegularPolygon(5, color=BLUE).scale(2)
-        pol5s = pol5.copy().set_fill(BLUE_C, 0.8).scale(0.8)
+        pol5s = pol5.copy().set_fill(BLUE_C, 0.8).scale(0.8).shift(DOWN*0.1)
         pol5t = Text("n = 5").next_to(pol5, DOWN)
 
         pol6 = RegularPolygon(6, color=GREEN)
@@ -344,20 +346,28 @@ class DecomposingSound(Scene):
 
 
 
-
+### Done in high quality
 class IntroducingFT(Scene):
     def construct(self):
+        self.wait(5)
         self.initialise_objects()
         self.play_scene()
+        self.unit_circle()
+        self.wait(5)
     
     def initialise_objects(self):
         self.tt = MathTex(r"\begin{array}{c} Time \\ f(t) \end{array}", color=YELLOW).shift(LEFT*3)
         self.tar = MathTex(r"\Longleftrightarrow", color=YELLOW)
         self.ft = MathTex(r"\begin{array}{c} Frequency \\ F(\omega) \end{array}", color=YELLOW).shift(RIGHT*3)
 
-        self.tft = Text("Fourier Transform", color=RED, font_size=64)
-        self.t2 = MathTex(FOURIER_TRANSFORM)
-        self.t2.set_color_by_tex("f", YELLOW)
+        self.t2 = MathTex(
+            r"F(\omega )",
+            r"=\int_{-\infty}^{\infty}",
+            r"f(t)",
+            r"e^{-i\omega t}",
+            r"\ dt",
+            color=YELLOW
+        )
 
         ax1 = Axes(
             x_range=(0, 2, 1), y_range=(-2, 2, 2),
@@ -383,12 +393,16 @@ class IntroducingFT(Scene):
     def play_scene(self):
         self.wait()
 
+        t = Title("Introducing the Fourier Transform", color=RED)
+        self.play(Write(t))
+        self.wait()
+
         self.play(FadeIn(self.tt, self.ft, self.tar))
         self.wait()
         self.play(self.tt.animate.shift(DL).shift(DOWN*2), self.ft.animate.shift(DR).shift(DOWN*2), self.tar.animate.shift(DOWN*3))
 
         self.play(Write(self.t2), run_time=2)
-        self.play(self.t2.animate.shift(UP*3))
+        self.play(FadeOut(t), self.t2.animate.shift(UP*3))
 
         self.play(Create(self.vg1[0]), Create(self.vg2[0]))
         self.play(Create(self.vg1[1]), Create(self.vg2[1]), Write(self.axs2l))
@@ -399,27 +413,185 @@ class IntroducingFT(Scene):
         self.play(FadeIn(ft_image))
         self.wait(8)
         self.remove(ft_image)
-        self.play(FadeOut(self.tt, self.ft, self.tar, self.t2, self.vg1, self.vg2, self.axs2l))
+        self.play(FadeOut(self.tt, self.ft, self.tar, self.vg1, self.vg2, self.axs2l))
 
-        jf_image = ImageMobject("src/jf.png").shift(LEFT*5).scale(0.5)
+        jf_image = ImageMobject("src/jf.png").scale(0.5)
         self.play(FadeIn(jf_image))
+        self.wait(4)
+        self.play(jf_image.animate.shift(LEFT*5))
+
+        ax1 = Axes(
+            x_range=(0, 2, 1), y_range=(-2, 2, 2),
+            x_length=5, y_length=4,
+            tips=False,
+            x_axis_config={"include_numbers": True}
+        )
+        axs1 = ax1.plot(lambda x: np.sin(2*2*PI*x - 0.5*PI), color=RED)
+        vg1 = VGroup(ax1, axs1)
+
+        vg1.shift(RIGHT*2).shift(DOWN*0.6).scale(1.5)
+        self.play(Create(ax1))
+        self.play(Create(axs1))
 
         self.wait(3)
-        self.vg1[0].shift(RIGHT*6).scale(1.5)
-        self.vg1[1].shift(RIGHT*6).scale(1.5)
-        self.play(Create(self.vg1[0]))
-        self.play(Create(self.vg1[1]))
 
-        self.wait(3)
+        dot1 = Dot(ax1.c2p(0.25, 1), color=BLUE)
+        dot1l = ax1.get_line_from_axis_to_point(0, ax1.c2p(0.25, 1))
+        dot2 = Dot(ax1.c2p(0.75, 1), color=BLUE)
+        dot2l = ax1.get_line_from_axis_to_point(0, ax1.c2p(0.75, 1))
 
-
-        dot1 = Dot(self.vg1[0].c2p(0.125, 1), color=BLUE)
-        dot1l = self.vg1[0].get_line_from_axis_to_point(0, self.vg1[0].c2p(0.125, 1))
-        dot2 = Dot(self.vg1[0].c2p(0.625, 1), color=BLUE)
-        dot2l = self.vg1[0].get_line_from_axis_to_point(0, self.vg1[0].c2p(0.625, 1))
         self.play(Create(dot1), Create(dot1l))
         self.play(Create(dot2), Create(dot2l))
 
+        tl = Line(ax1.c2p(0.25, 1), ax1.c2p(0.75, 1), color=BLUE)
+        tlt = MathTex("T", color=BLUE).next_to(tl, UP, buff=0.1)
+        al = Arrow(ax1.c2p(1.25, -0.17), ax1.c2p(1.25, 1.15), color=BLUE)
+        alt = MathTex("A", color=BLUE).next_to(al, RIGHT, buff=0.2)
+        pll = ax1.get_line_from_axis_to_point(0, ax1.c2p(0.25, -1))
+        pl = Arrow(ax1.c2p(-0.07, -1), ax1.c2p(0.3, -1), color=BLUE)
+        plt = MathTex("\\theta", color=BLUE).next_to(pl, DOWN, buff=0.1)
+
+        self.play(Create(al), Write(alt))
+        self.play(Create(tl), Write(tlt))
+        self.play(Create(pl), Create(pll), Write(plt))
+
         self.wait(5)
+
+        self.play(FadeOut(jf_image, shift=LEFT*2), FadeOut(dot1, dot1l, dot2, dot2l, vg1, tl, tlt, al, alt, pl, pll, plt, shift=RIGHT*4))
+
+        self.wait()
+
+        ef = MathTex(EULERS_FORMULA, color=BLUE)
+        efr = SurroundingRectangle(ef, color=YELLOW)
+        evg = VGroup(ef, efr)
+
+        self.play(Write(ef), Create(efr), run_time=2)
+        self.play(evg.animate.shift(UR*3).shift(RIGHT), self.t2.animate.shift(LEFT*4))
+        
+        ftr = SurroundingRectangle(self.t2[3], color=RED)
+        self.play(Create(ftr))
+
+        e = self.t2[3].copy().set_color(WHITE)
+        eff = MathTex(r"&=\cos{(-\omega t)}+i\sin{(-\omega t)} \\", r"&= \cos{(\omega t)}-i\sin{(\omega t)}").shift(UP*0.6)
+        self.play(e.animate.shift(LEFT*2).shift(DOWN*2))
+        self.play(Write(eff), run_time=4)
+
+        self.wait(2)
+
+        ec = MathTex(r"e^{-i\omega t}", r"=\cos{(\omega t)}-i\sin{(\omega t)}").shift(UR*2).shift(RIGHT)
+        self.play(FadeOut(eff[0]), ReplacementTransform(e, ec[0]), ReplacementTransform(eff[1], ec[1]))
+        ecr = SurroundingRectangle(ec, color=YELLOW)
+        self.play(Create(ecr))
+        self.play(Uncreate(ftr))
+
+        #
+
+        ftr1 = SurroundingRectangle(self.t2[2:-1], color=RED)
+        self.play(Create(ftr1))
+
+        f2 = MathTex(
+            r"F(\omega)&=\int_{-\infty}^{\infty}f(t)[\cos({\omega})-i\sin({\omega t})] \ dt \\",
+            r"F(\omega)&=\int_{-\infty}^{\infty}f(t)\cos({\omega}) \ dt-i\int_{-\infty}^{\infty}f(t)\sin({\omega t}) \ dt"
+        )
+        self.play(Write(f2[0]), run_time=3)
+        self.play(Write(f2[1]), run_time=3)
+        
+        self.wait(3)
+
+        self.play(FadeOut(f2, ec, ecr, shift=UP*3))
+        self.play(Uncreate(ftr1))
+
+        arg_dia = NumberPlane(
+            (-20, 20, 1), (-20, 20, 1),
+            background_line_style={
+                "stroke_opacity": 0.3
+            }
+        ).add_coordinates().shift(DL*3).shift(UP*1.5)
+        y_label = arg_dia.get_y_axis_label(Tex("Im", color=RED)).shift(DL*1.5)
+        x_label = arg_dia.get_x_axis_label(Tex("Re", color=RED))
+        labels = VGroup(y_label, x_label)
+
+        self.play(Create(arg_dia), Create(labels), run_time=2)
+
+        yl = Line(arg_dia.c2p(5, 0), arg_dia.c2p(5, 3))
+        ylt = MathTex(r"\int_{-\infty}^{\infty}f(t)\sin({\omega t})", color=YELLOW).next_to(yl, RIGHT, buff=0.3)
+        xl = Line(arg_dia.c2p(0, 0), arg_dia.c2p(5, 3))
+        xlt = MathTex(r"\int_{-\infty}^{\infty}f(t)\cos({\omega})", color=YELLOW).next_to(yl, DOWN, buff=0.3).shift(LEFT*2.2)
+        theta = MathTex(r"\theta", color=BLUE).shift(DL*1.1).shift(LEFT*0.7)
+        fmag = MathTex(r"|f(\omega)|", color=YELLOW).next_to(xl, UP, buff=0.3).shift(DOWN)
+        
+        self.play(Create(yl), Create(xl), Create(ylt), Create(xlt), Write(theta), Write(fmag))
+
+        self.wait(5)
+
+        mod_arg_image = ImageMobject("src/mod_arg.png").scale(0.8).shift(RIGHT*3).shift(DOWN*2)
+        imgsr = SurroundingRectangle(mod_arg_image, color=YELLOW)
+        self.play(FadeIn(mod_arg_image), Create(imgsr))
+
+        self.wait(8)
+
+        self.play(FadeOut(arg_dia, imgsr, mod_arg_image, theta, fmag, yl, ylt, xl, xlt, labels, self.t2, ef, efr))
+
+        self.wait()
+
+        # ===== =====   ===== =====
+    
+    def unit_circle(self):
+        n = ValueTracker(0)
+
+        unit_circle = Circle(1, color=WHITE)
+        point = always_redraw(
+            lambda: Dot(color=BLUE, radius=.1, stroke_width=0).move_to(unit_circle.point_at_angle(n.get_value()*DEGREES))
+        )
+        hline = always_redraw(
+            lambda: Line(unit_circle.get_center(), point)
+        )
+        yline = always_redraw(
+            lambda: Line((point.get_x(), unit_circle.get_y(), 0), point, color=YELLOW)
+        )
+        xline = always_redraw(
+            lambda: Line(unit_circle.get_center(), (point.get_x(), unit_circle.get_y(), 0), color=RED)
+        )
+
+        self.play(Create(unit_circle), Create(point), Create(hline), Create(yline), Create(xline))
+        eulers = MathTex(r"e^{i\theta}", color=YELLOW).next_to(unit_circle, UP)
+        self.play(FadeIn(eulers, shift=UP))
+        circ = VGroup(unit_circle, eulers)
+
+        self.play(circ.animate.shift(LEFT*5).shift(UP*2))
+        self.wait(5)
+
+
+        sin_ax = Axes(
+            x_range=(0, 2*PI, PI), y_range=(-1, 1, 1),
+            x_length=5, y_length=2,
+            tips=False,
+        ).shift(UP*2)
+        sin_gr = sin_ax.plot(lambda x: np.sin(x), color=RED)
+        sin_ogr = sin_ax.plot(lambda x: np.sin(-x), color=RED)
+
+        cos_ax = Axes(
+            x_range=(0, 2*PI, PI), y_range=(-1, 1, 1),
+            x_length=5, y_length=2,
+            tips=False,
+        ).shift(DOWN*1.5).shift(LEFT*5).rotate(-90*DEGREES)
+        cos_gr = cos_ax.plot(lambda x: np.cos(x), color=YELLOW)
+
+
+        self.play(Create(sin_ax), Create(cos_ax))
+        self.play(n.animate.set_value(360), Create(sin_gr), Create(cos_gr), rate_func=linear, run_time=10)
+        self.wait(3)
+        self.play(FadeOut(sin_gr, cos_gr))
+        self.play(n.animate.set_value(0), Create(sin_ogr), Create(cos_gr), rate_func=linear, run_time=10)
+
+        st = MathTex(r"\sin({-\theta})&=-\sin({\theta}) \\", r"\cos({\theta})&=\cos({-\theta})", color=BLUE).shift(RIGHT).shift(DOWN)
+        sr = SurroundingRectangle(st, color=YELLOW, buff=MED_LARGE_BUFF)
+
+        self.play(Write(st[0]), run_time=2)
+        self.wait()
+        self.play(Write(st[1]), run_time=2)
+        self.wait()
+        self.play(Create(sr), rate_func=rate_functions.ease_in_out_quint, run_time=3)
+
 
 
